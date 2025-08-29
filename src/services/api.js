f56d244faf375e1c85fb3c01d9e7fe258896afac
HARSHITA SRIVASTAVA <harshitasrivastava@HARSHITAs-MacBook-Air.local>
2025-08-29 20:36:38 +0530

    Use env-based API base URL (VITE_API_URL) and update Axios baseURL

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
+import { API_BASE_URL } from '../utils/constants';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080', // Backend URL
+  baseURL: API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
